a9325aa06837ead5e2160e23c5e30c1cb60a594b
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-27 02:04:51 -0600

    Quick fix

--- zed_ws/src/zed-ros2-wrapper/zed_wrapper/launch/zed_camera.launch.py
+++ zed_ws/src/zed-ros2-wrapper/zed_wrapper/launch/zed_camera.launch.py
@@ -347,14 +347,12 @@ def generate_launch_description():
                 choices=['true', 'false']),
             DeclareLaunchArgument(
                 'publish_tf',
-                # Changed default value to 'false' to avoid TF conflicts - Nelson Durrant, Mar 2025
-                default_value='false',
+                default_value='true',
                 description='Enable publication of the `odom -> camera_link` TF.',
                 choices=['true', 'false']),
             DeclareLaunchArgument(
                 'publish_map_tf',
-                # Changed default value to 'false' to avoid TF conflicts - Nelson Durrant, Mar 2025
-                default_value='false',
+                default_value='true',
                 description='Enable publication of the `map -> odom` TF. Note: Ignored if `publish_tf` is False.',
                 choices=['true', 'false']),
             DeclareLaunchArgument(
